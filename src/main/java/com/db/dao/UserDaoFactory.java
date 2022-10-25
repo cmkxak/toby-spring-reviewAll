7134d565deb000fdfe9b8086402a7ca5d934fba4
cmkxak <gcm466@naver.com>
2022-10-25 10:54:03 +0900

    fix : UserDao로 클래스명 변경 및 Factory 클래스 생성

--- src/main/java/com/db/dao/UserDaoFactory.java
+++ src/main/java/com/db/dao/UserDaoFactory.java
@@ -1,9 +1,6 @@
 package com.db.dao;
 
+
 public class UserDaoFactory {
 
-    public UserDao05WithInterface userDao() {
-        MySqlConnectionMaker mySqlConnectionMaker = new MySqlConnectionMaker();
-        return new UserDao05WithInterface(mySqlConnectionMaker);
-    }
 }
