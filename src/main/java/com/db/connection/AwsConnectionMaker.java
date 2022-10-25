53a95437b411371340406f4ed439431c86a142d5
cmkxak <gcm466@naver.com>
2022-10-25 11:19:15 +0900

    fix : 인터페이스 구현하도록 수정 및 오타 수정

--- src/main/java/com/db/connection/AwsConnectionMaker.java
+++ src/main/java/com/db/connection/AwsConnectionMaker.java
@@ -5,12 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
 
-public class AwsConnectionMaker {
+public class AwsConnectionMaker implements ConnectionMaker {
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Map<String, String> env = System.getenv();
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                env.get("DB_HOST"), env.get("DB_ROOT"), env.get("DB_PASSWORD"));
+        Connection c = DriverManager.getConnection(env.get("DB_HOST"), env.get("DB_USER"), env.get("DB_PASSWORD"));
         return c;
     }
 }
