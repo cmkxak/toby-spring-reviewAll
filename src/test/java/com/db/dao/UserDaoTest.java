c2a8cc47e454626beb5d2c1bd5520cabf25a6bff
cmkxak <gcm466@naver.com>
2022-10-25 11:19:41 +0900

    fix : 불필요한 테스트 코드 삭제 및 테스트 코드 재구성

---
+++ src/test/java/com/db/dao/UserDaoTest.java
@@ -0,0 +1,39 @@
+package com.db.dao;
+
+import com.db.connection.AwsConnectionMaker;
+import com.db.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setUp(){
+        userDao = new UserDaoFactory().awsUserDao();
+        user1 = new User("3", "홍길동", "1234");
+        user2 = new User("4", "홍길수", "12346");
+    }
+
+    @Test
+    @DisplayName("add 테스트")
+    void add_Test() throws SQLException, ClassNotFoundException {
+        userDao.add(user1);
+        userDao.add(user2);
+
+        User findUser1 = userDao.findById(user1.getId());
+        User findUser2 = userDao.findById(user2.getId());
+
+        assertEquals(user1.getName(), findUser1.getName());
+        assertEquals(user2.getName(), findUser2.getName());
+    }
+
+}
