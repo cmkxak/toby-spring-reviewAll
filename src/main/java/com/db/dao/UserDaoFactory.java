233c20f80738db313f8c89566fa7c8eff23c74ad
cmkxak <gcm466@naver.com>
2022-10-25 11:22:01 +0900

    fix : 팩토리 클래스 생성

--- src/main/java/com/db/dao/UserDaoFactory.java
+++ src/main/java/com/db/dao/UserDaoFactory.java
@@ -1,6 +1,17 @@
 package com.db.dao;
 
 
-public class UserDaoFactory {
+import com.db.connection.AwsConnectionMaker;
+import com.db.connection.LocalConnectionMaker;
 
+public class UserDaoFactory {
+    public UserDao awsUserDao(){
+        UserDao userDao = new UserDao(new AwsConnectionMaker());
+        return userDao;
+    }
+
+    public UserDao localUserDao(){
+        UserDao userDao = new UserDao(new LocalConnectionMaker());
+        return userDao;
+    }
 }
