758afe070131aa2fdbe807fee69c47df0fda22ce
cmkxak <gcm466@naver.com>
2022-10-25 14:18:54 +0900

    fix : DataSource 도입

--- src/main/java/com/db/dao/UserDaoFactory.java
+++ src/main/java/com/db/dao/UserDaoFactory.java
@@ -1,22 +1,42 @@
 package com.db.dao;
 
-
-import com.db.connection.AwsConnectionMaker;
-import com.db.connection.LocalConnectionMaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import javax.sql.DataSource;
+import java.util.Map;
 
 @Configuration
 public class UserDaoFactory {
-
     @Bean
     public UserDao awsUserDao(){
-        UserDao userDao = new UserDao(new AwsConnectionMaker());
+        UserDao userDao = new UserDao(awsDataSource());
         return userDao;
     }
     @Bean
     public UserDao localUserDao(){
-        UserDao userDao = new UserDao(new LocalConnectionMaker());
+        UserDao userDao = new UserDao(localDataSource());
         return userDao;
+    }
+
+    @Bean
+    public DataSource awsDataSource(){
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
+    }
+
+    @Bean
+    public DataSource localDataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl("localhost");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+        return dataSource;
     }
 }
