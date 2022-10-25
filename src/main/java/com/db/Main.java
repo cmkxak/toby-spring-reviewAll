6bf891c14236e670a9fddabe7c36a53d1569b896
cmkxak <gcm466@naver.com>
2022-10-25 10:55:41 +0900

    fix : 불필요한 코드 제거

--- src/main/java/com/db/Main.java
+++ src/main/java/com/db/Main.java
@@ -1,17 +1,7 @@
 package com.db;
 
-import com.db.dao.MySqlConnectionMaker;
-import com.db.dao.UserDao05WithInterface;
-import com.db.dao.UserDaoFactory2;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        AnnotationConfigApplicationContext ctx =
-                new AnnotationConfigApplicationContext(UserDaoFactory2.class);
-        UserDao05WithInterface userDao = ctx.getBean("userDao05Interface", UserDao05WithInterface.class);
-        userDao.add();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
