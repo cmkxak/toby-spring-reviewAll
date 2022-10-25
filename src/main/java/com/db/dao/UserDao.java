f1f4942f569a982c73c1072c269da5a1bcf5abd8
cmkxak <gcm466@naver.com>
2022-10-25 14:18:29 +0900

    fix : JdbcContext에 의존하도록 수정

--- src/main/java/com/db/dao/UserDao.java
+++ src/main/java/com/db/dao/UserDao.java
@@ -1,22 +1,23 @@
 package com.db.dao;
 
-import com.db.connection.ConnectionMaker;
+import com.db.context.JdbcContext;
 import com.db.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private JdbcContext jdbcContext;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource datasource) {
+        this.jdbcContext = new JdbcContext(datasource);
     }
 
     public void add(User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("INSERT INTO users values (?,?,?)");
@@ -28,163 +29,12 @@ public class UserDao {
         });
     }
 
-    public User findById(String id) throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-
-        try {
-            c = connectionMaker.getConnection();
-            ps = c.prepareStatement("select * from users where id = ?");
-            ps.setString(1, id);
-
-            rs = ps.executeQuery();
-
-            if (rs.next()) {
-                user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-            }
-            return user;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
-    public List<User> findAll() {
-        Connection c = null;
-        Statement statement = null;
-        ResultSet rs = null;
-
-        List<User> userList = new ArrayList<>();
-        try {
-            c = connectionMaker.getConnection();
-
-            statement = c.createStatement();
-
-            rs = statement.executeQuery("select * from users");
-
-            while (rs.next()) {
-                User user = new User(rs.getString("id"),
-                        rs.getString("name"), rs.getString("password"));
-                userList.add(user);
-            }
-            return userList;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
-    public int getCount() throws SQLException, ClassNotFoundException {
-        Connection c = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            c = connectionMaker.getConnection();
-            pstmt = c.prepareStatement("select count(*) from users");
-            rs = pstmt.executeQuery();
-            rs.next();
-
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
     public void deleteAll(){
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 return c.prepareStatement("delete from users");
             }
         });
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy){
-        Connection c = null;
-        PreparedStatement pstmt = null;
-        try {
-            c = connectionMaker.getConnection();
-            pstmt = statementStrategy.makeStatement(c);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
     }
 }
