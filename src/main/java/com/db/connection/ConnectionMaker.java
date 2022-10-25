3426ecb22b605fbced4c89ce7a916ab20d16cd57
cmkxak <gcm466@naver.com>
2022-10-25 14:16:45 +0900

    fix : ConnectionMaker 인터페이스와 구현 클래스 삭제

--- src/main/java/com/db/connection/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.db.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    Connection getConnection() throws ClassNotFoundException, SQLException;
-}
